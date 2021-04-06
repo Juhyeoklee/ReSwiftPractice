d86f5307dbbc1a0235c8ed7e0d0f409e86e4ceeb
juhyeok.lee <juhyeok.lee@streami.co>
2021-04-06 15:53:08 +0900

    init Project

---
+++ ReTest/AppState.swift
@@ -0,0 +1,17 @@
+//
+//  AppState.swift
+//  ReTest
+//
+//  Created by juhyeok.lee on 2021/04/06.
+//
+
+import Foundation
+
+import ReSwift
+
+
+struct AppState: StateType {
+    var counter: Int = 0
+}
+
+
